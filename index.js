eee4195d8476ec3f8f4ef4df16036a8cb7e79182
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-12-22 17:21:20 +0000

    question, example, constraints

---
+++ index.js
@@ -0,0 +1,10 @@
+Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+
+ Example 1:
+  Input: s = "abab"
+  Output: true
+  Explanation: It is the substring "ab" twice.
+
+  Constraints:
+    1 <= s.length <= 104
+    s consists of lowercase English letters.
