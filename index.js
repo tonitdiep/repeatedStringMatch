4b8b603d1a002052f26ce1e349812d6a2c71c341
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-12-22 17:44:57 +0000

    solution

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ Given a string s, check if it can be constructed by taking a substring of it and
   Constraints:
     1 <= s.length <= 104
     s consists of lowercase English letters.
+
+    /**
+ * @param {string} s
+ * @return {boolean}
+ */
+var repeatedSubstringPattern = function(s) {
+    let subStringChecker = "";
+    for (let i = 0; i < Math.floor(s.length/2); i++){
+        subStringChecker += s[i];
+        if (subStringChecker.repeat(s.length/subStringChecker.length) === s) return true;
+    }
+    return false;
+       
+};
